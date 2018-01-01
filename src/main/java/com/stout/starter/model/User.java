9599968dc6fc67221e6b871b42f6a58effd42d34
Dylan-Stout <dylan.d.stout@gmail.com>
2018-01-01 14:05:35 -0700

    hibernate addition
    adding in hibernate for orm

--- src/main/java/com/stout/starter/model/User.java
+++ src/main/java/com/stout/starter/model/User.java
@@ -1,5 +1,78 @@
 package com.stout.starter.model;
 
-public class User {
+import java.util.ArrayList;
+import java.util.Collection;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO)
+	@Column(name="userid")
+	private int id; 
+
+	@Column(name="username")
+	private String username;
+	
+	@Column(name="displayname")
+	private String displayname;
+	
+	@OneToMany
+	@JoinColumn(name="roleid")
+	private Collection<Role> roles = new ArrayList<Role>(); 
+	
+	@Column(name="address")
+	private String address;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getDisplayname() {
+		return displayname;
+	}
+
+	public void setDisplayname(String displayname) {
+		this.displayname = displayname;
+	}
+
+	public Collection<Role> getRole() {
+		return roles;
+	}
+
+	public void setRole(Collection<Role> role) {
+		this.roles = role;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	} 
+	
+	
+	
+	
 }
--- src/main/java/com/stout/starter/service/UserProfileService.java
+++ src/main/java/com/stout/starter/service/UserProfileService.java
@@ -1,5 +1,32 @@
 package com.stout.starter.service;
 
+import com.stout.starter.model.User;
+
 public class UserProfileService {
+	
+	/**
+	 * Base user authentication logic. Use hashing alg to check password is correct and
+	 * if so, return the appropriate user object. 
+	 * 
+	 * @param userName
+	 * @param password
+	 * @return if success - User object, if fail - null 
+	 */
+	public User authenticateUser(String userName, String password)
+	{ 
+		return null; // TODO implement model layer for password authentication
+	}
+	
+	
+	/**
+	 * Logout user ---- 
+	 * 
+	 * 
+	 * @param user
+	 */
+	public void logoutUser(User user)
+	{ 
+		//TODO implement logout logic 
+	}
 
 }
